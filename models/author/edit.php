96c3e9fe288497073220d4192cf02c30dfbc0920
Nemanja-Jovicic <helmserbon@gmail.com>
2025-10-24 16:52:23 +0200

    edit file for author

---
+++ models/author/edit.php
@@ -0,0 +1,14 @@
+<?php 
+header("Content-type:application/json");
+if($_SERVER["REQUEST_METHOD"] === "GET"){
+    include '../functions.php';
+    $id = $_GET['id'];
+    try {
+        require_once '../../config/connection.php';
+        echo json_encode(['data' => $getOneAuthor($id)]);
+    } catch (PDOException $th) {
+        intervalError($th->getMessage());
+    }
+}else{
+    pageNotFound();
+}
