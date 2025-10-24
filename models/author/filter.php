aa357748b195f6ca6a3d6938ca0fa2f6892aeeee
Nemanja-Jovicic <helmserbon@gmail.com>
2025-10-25 00:49:20 +0200

    Pagination feature for author entity is added

---
+++ models/author/filter.php
@@ -0,0 +1,19 @@
+<?php
+header("Content-type:application/json");
+if ($_SERVER["REQUEST_METHOD"] === "GET") {
+    include '../functions.php';
+    $link = $_GET['link'];
+    try {
+        require_once '../../config/connection.php';
+        echo json_encode([
+            'data' => [
+                'data' => getAllAuthors($link),
+                'pages' => paginationAuthor()
+            ]
+        ]);
+    } catch (PDOException $th) {
+        intervalError($th->getMessage());
+    }
+} else {
+    pageNotFound();
+}
