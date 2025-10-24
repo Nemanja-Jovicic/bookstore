496774779e6f3632c0ad92b7b159c2ba31637a15
Nemanja-Jovicic <helmserbon@gmail.com>
2025-10-25 00:50:29 +0200

    store functionality

---
+++ models/author/store.php
@@ -0,0 +1,34 @@
+<?php
+header("Content-type:application/json");
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+    include '../functions.php';
+    include '../validations.php';
+
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $link = isset($_POST['link']) ? $_POST['link'] : 0;
+
+    $validation = authorFormValidation($first_name, $last_name);
+    if (count($validation) > 0) {
+        validationError($validation);
+    } else {
+        try {
+            require_once '../../config/connection.php';
+            storeNewAuthor($first_name, $last_name);
+            echo json_encode([
+                'data' => [
+                    'data' => getAllAuthors($link),
+                    'link' => $link,
+                    'pages' => paginationAuthor()
+                ],
+                'message' => 'Novi author je dodat'
+            ]);
+            opertionSuccess();
+        } catch (PDOException $th) {
+            intervalError($th->getMessage());
+        }
+    }
+} else {
+    pageNotFound();
+}
