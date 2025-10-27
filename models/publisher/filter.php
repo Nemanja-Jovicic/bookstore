77ea8d8fcbbb1a599f8fd2aab1c6d2b7f718f9d8
Nemanja-Jovicic <helmserbon@gmail.com>
2025-10-27 23:05:46 +0100

    pagination for filter

---
+++ models/publisher/filter.php
@@ -0,0 +1,21 @@
+<?php
+header("Content-type:application/json");
+if($_SERVER["REQUEST_METHOD"] === "GET"){
+    include '../functions.php';
+    $link = $_GET['link'];
+    try {
+        require_once '../../config/connection.php';
+         echo json_encode([
+            'data' => [
+                'data' => getAllPublishers($link),
+                'pages' => paginationPublisher(),
+                'link' => $link
+            ]
+        ]);
+    } catch (PDOException $th) {
+        intervalError($th->getMessage());
+    }
+}
+else {
+    pageNotFound();
+}
