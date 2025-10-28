5abd29072fd3cebbc42b9ca41c961d038f25c021
Nemanja-Jovicic <helmserbon@gmail.com>
2025-10-28 21:31:44 +0100

    validation functions

--- assets/js/validation_functions.js
+++ assets/js/validation_functions.js
@@ -7,3 +7,44 @@ const inputFieldValidation = (elementId, elementRegex, elementErrorId,
         removeValidationErrorMessage(elementErrorId)
     }
 }
+const selectFieldValidation = (elementId, errorElementId, errorMessage, errorArray) => {
+    if(parseInt(elementId) === 0){
+        createValidationErrorMessage(errorElementId, errorMessage)
+        errorArray.push(errorMessage)
+    }else{
+        removeValidationErrorMessage(errorElementId)
+    }
+}
+const checkBoxFieldValidation = (elementArray, errorElementId, errorMessage, errorArray) => {
+    if(elementArray.length === 0){
+        createValidationErrorMessage(errorElementId, errorMessage)
+        errorArray.push(errorMessage)
+    }else{
+        removeValidationErrorMessage(errorElementId)
+    }
+}
+const inputFileFieldValidation = (file, errorElementId, errorMessages, errorArray) => {
+    const [emptyMessage, typeMessage, sizeMessage] = errorMessages
+    if(file.length === 0){
+        createValidationErrorMessage(errorElementId, emptyMessage)
+        errorArray.push(emptyMessage)
+    }else{
+        console.log(file)
+        file = file[0]
+
+        const fileSize = file['size']
+        const fileType = file['type'];
+
+        const validFormats = ['image/png', 'image/jpeg', 'image/jpg'] 
+
+        if(!validFormats.includes(fileType)){
+            errorArray.push(typeMessage)
+            createValidationErrorMessage(errorElementId, typeMessage)
+        }else if(fileSize > 3 * 1024 * 1024){
+            errorArray.push(sizeMessage)
+            createValidationErrorMessage(errorElementId, sizeMessage)
+        }else{
+            removeValidationErrorMessage(errorElementId)
+        }
+    }
+}
