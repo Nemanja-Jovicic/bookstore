0129cf3ba982287f77fe08256ac38e0a70838aa1
Nemanja-Jovicic <helmserbon@gmail.com>
2025-10-28 21:31:14 +0100

    validation functions

--- models/validations.php
+++ models/validations.php
@@ -13,9 +13,6 @@ function registerValidation($first_name, $last_name, $email, $password)
 
     return $errorArray;
 }
-
-
-
 function loginValidation($email, $password)
 {
     $errorArray = [];
@@ -24,4 +21,53 @@ function loginValidation($email, $password)
     inputFieldValidation("Password nije u redu", $errorArray, $password, $regPassword);
 
     return $errorArray;
+}
+function authorFormValidation($first_name, $last_name)
+{
+    $errorArray = [];
+    $regFirstLastName = "/\b([A-ZÀ-ÿČĆŽŠĐ][-,a-zčćžšđ. ']+[ ]*)+/";
+    inputFieldValidation("Ime nije u redu!", $errorArray, $first_name, $regFirstLastName);
+    inputFieldValidation("Prezime nije u redu!", $errorArray, $last_name, $regFirstLastName);
+
+    return $errorArray;
+}
+function publisherValidation($name)
+{
+    $errorArray = [];
+    $regName = "/^[A-Z][a-z]{1,}(\s[A-Z][a-z]{1,}|\s[a-z]{1,})*$/";
+    inputFieldValidation("Naziv izdavaca nije u redu!", $errorArray, $name, $regName);
+    return $errorArray;
+}
+function bookFormValidation($name)
+{
+    $errors = [];
+    $regBookName = "/^[A-ZČĆŠĐŽa-zčćšđž0-9 ,.'\":;!?()-]+$/";
+    inputFieldValidation("Naziv knjige nije  u redu!", $errors, $name, $regBookName);
+    return $errors;
+}
+function editionValidation($publisher, $cover_type, $letter_type, $genres, $authors, $price, $description, $image = '', $id = '')
+{
+    $errors = [];
+
+
+    $regPrice = "/^[1-9]{1}[\d]{2,5}$/";
+    $regDescription = "/^[A-Za-zČĆŽŠĐčćžšđ0-9\s.,!?()'\"-]{10,500}$/";
+
+
+    inputSelectValidation($publisher, "Morate izabrati izdavaca!", $errors);
+    inputSelectValidation($cover_type, "Morate odabrati tip korice!", $errors);
+    inputSelectValidation($letter_type, "Morate odabrati tip pisma!", $errors);
+    checkBoxValidation($authors, "Morate odabrati barem jednog autora!", $errors);
+    checkBoxValidation($genres, "Morate odabrati barem jedan zanr!", $errors);
+
+
+    inputFieldValidation("Cena nije u redu!", $errors, $price, $regPrice);
+    inputFieldValidation("Opis knjige nije u redu!", $errors, $description, $regDescription);
+
+    if ($id === '') {
+        inputFileValidaiton($image, ["Morate odabrati sliku!", "Slika nije u dobrom formatu!", "Velicina slike nije u redu!"], $errors);
+    } else if ($image !== '') {
+        inputFileValidaiton($image, ["Morate odabrati sliku!", "Slika nije u dobrom formatu!", "Velicina slike nije u redu!"], $errors);
+    }
+    return $errors;
 }
