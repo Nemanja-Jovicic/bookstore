b86e68809aa94c69c057991e97d3f5fab7a05982
Nemanja-Jovicic <helmserbon@gmail.com>
2025-10-28 22:00:04 +0100

    utility functions

--- assets/js/utility_functions.js
+++ assets/js/utility_functions.js
@@ -47,12 +47,55 @@ const sendPostRequest = async (url, data) => {
             ? (window.location.href = "admin.php")
             : (window.location.href = "index.php");
         }
+      } else {
+        if (file === "delete") {
+          printAfterSoftDelete(directory, responseData);
+        } else if (file === "store") {
+          clearForm(directory);
+          responseMessages(urlRequest.data.message, "success");
+          printAllData(responseData.data, directory, responseData.link);
+          if(directory !== 'edition'){
+            printPagination(responseData.pages, directory, responseData.link)
+          }
+        } else if (file === "update") {
+          clearForm(directory);
+          printData(
+            responseData,
+            parseInt(responseData.id) - 1,
+            directory,
+            "update"
+          );
+        }
       }
     } else {
       responseMessages(urlRequest.response.data.error, "danger");
     }
   } catch (error) {
-    responseMessages(error.response.data.error, "danger");
+    responseMessages(error.response?.data.error, "danger");
+  }
+};
+const sendGetRequest = async (url, data = "") => {
+  const splitedUrl = url.split("/");
+  const entity = splitedUrl[splitedUrl.length - 2];
+  const action = splitedUrl[splitedUrl.length - 1].split(".")[0];
+
+  const urlParams = new URLSearchParams(data).toString();
+  try {
+    const request = await axios.get(`${url}?${urlParams}`);
+    const responseData = await request.data;
+    if (request.status >= 200 && request.status <= 300) {
+      if (action === "edit") {
+        fillForm(responseData.data, entity);
+      } else if (action === "filter") {
+    
+        printAllData(responseData.data.data, entity, responseData.data.link);
+        printPagination(responseData.data.pages, entity, responseData.data.link)
+      }
+    } else {
+      responseMessages(request.response.data.error, "danger");
+    }
+  } catch (error) {
+    responseMessages(error.response?.data.error, "danger");
   }
 };
 
@@ -66,4 +109,26 @@ const responseMessages = (message, color) => {
   `;
   const element = document.querySelector("#message");
   element.innerHTML = messageEl;
+};
+const removeFromObj = (obj) => {
+  for (const key of Array.from(obj.keys())) {
+    obj.delete(key);
+  }
+};
+const formatDate = (dateTime) => {
+  const splitedTimeDate = dateTime.split(" ");
+  const time = splitedTimeDate[1] !== undefined ? splitedTimeDate[1] : "";
+  const date = splitedTimeDate[0].split("-");
+  return `${date[2]}/${date[1]}/${date[0]} ${time}`;
+}
+const printPagination = (page, entity, activePage = 0) => {
+  let content = "";
+
+  content += `<ul class='pagination'>`;
+  for (let i = 0; i < page; i++) {
+    content += `<li class="page-item "><a class="page-link  ${parseInt(activePage) === i ? 'active' : ''} page-${entity}"`; 
+    content += `"href="#" data-link="${i}">${i + 1}</a></li>`;
+  }
+  content += "</ul>";
+  document.querySelector(`#${entity}_pagination`).innerHTML = content;
 };
