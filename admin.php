3695b0c0e01c5693c7a3714a51e6e4d4fc5b3b3d
Nemanja-Jovicic <helmserbon@gmail.com>
2025-10-28 22:01:40 +0100

    routing between pages and static files

--- admin.php
+++ admin.php
@@ -1,5 +1,18 @@
 <?php 
 session_start();
+require_once 'config/connection.php';
+include 'models/functions.php';
+
 include 'includes/parts/head.php';
 include 'includes/parts/navigation.php';
+if(isset($_GET['page'])){
+    $page = $_GET['page'];
+    if(file_exists("includes/pages/admin/$page.php")){
+        include "includes/pages/admin/$page.php";
+    }else{
+        include 'includes/pages/global/error.php';
+    }
+}else{
+    include 'includes/pages/admin/home.php';
+}
 include 'includes/parts/footer.php';
